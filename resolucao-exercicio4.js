f336250e02156e3dcb31a241ae19935d5350e898
Pamela Orsolin <112989709+pamela-orsolin@users.noreply.github.com>
2022-10-22 16:25:06 -0300

    Add files via upload

---
+++ resolucao-exercicio4.js
@@ -0,0 +1,17 @@
+//Crie um programa que verifique se um determinado número é impar ou par!
+//Caso seja ímpar retorne "O número X é ímpar"
+//Caso seja par retorne "O número X é ímpar"
+
+//Dica do sucesso: todo número par é divisível por 2
+
+let numero =19
+
+if (numero % 2 == 0){
+
+    console.log("O número é par")
+    
+    }else{
+    
+     console.log("O número é ímpar")
+    
+    }
