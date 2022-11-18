ce8cee1e92293a3b10120ce0cf154082dc55796e
Pamela Orsolin <112989709+pamela-orsolin@users.noreply.github.com>
2022-11-17 22:16:13 -0300

    Add files via upload

---
+++ Arrays/array1.js
@@ -0,0 +1,12 @@
+/* Leia um vetor de 12 posições e em seguida leia 
+dois valores X e Y quaisquer correspondentes a duas
+posições no vetor. Ao final seu programa devera escrever
+a soma dos valores nas respectivas posições 
+*/
+
+let dozePosicoes = [21, 54, 44, 67, 47, 14, 21, 79, 16, 85, 32, 77]
+console.log(dozePosicoes.length)
+console.log(dozePosicoes[2])
+console.log(dozePosicoes[11])
+let somaNumeros = dozePosicoes[5]+dozePosicoes[11]
+console.log(somaNumeros)
